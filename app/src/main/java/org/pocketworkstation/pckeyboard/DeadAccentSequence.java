27114c574fcb1098b8eeac8f3d0a5a69feeae919
Alain Knaff <alain@knaff.lu>
2026-01-17 22:58:09 +0100

    Fixed 2 minor warnings

--- app/src/main/java/org/pocketworkstation/pckeyboard/DeadAccentSequence.java
+++ app/src/main/java/org/pocketworkstation/pckeyboard/DeadAccentSequence.java
@@ -73,10 +73,7 @@ public class DeadAccentSequence extends ComposeSequence {
 
 	private static String doNormalise(String input)
 	{
-		if (Build.VERSION.SDK_INT >= 9) {
-			return Normalizer.normalize(input, Normalizer.Form.NFC);
-		}
-		return input;
+		return Normalizer.normalize(input, Normalizer.Form.NFC);
 	}
 
     public static String normalize(String input) {
